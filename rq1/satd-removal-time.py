c1623e225198c17a625783c3e9239ba4abcdc82a
Hisham Kidwai <HishamSurgeon@gmail.com>
2024-08-01 22:37:00 -0500

    Use simpler code Same as other code for cdf

--- rq1/satd-removal-time.py
+++ rq1/satd-removal-time.py
@@ -1,74 +1,88 @@
-import pandas as pd
+import os
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 
-def get_removal_times(df):
+# SRCDIR = "../../software-evolution/tech-debt-results/"
+SRCDIR = '/home/hisham-kidwai/Documents/HISHAM/Research/Tech-Debt/csv-files-satd/'
+
+
+
+def build_indexes(line):
+    indexes = {}
+    data = line.strip().split("\t")
+    for i in range(len(data)):
+        indexes[data[i]] = i
+    return indexes    
+
+def check_satd(satd):
+    for s in satd:
+        s = int(s)
+        if s == 1:
+            return True
+    return False
+            
+def process():
     removal_times = []
-    removal_indices = []
 
-    for index, row in df.iterrows():
-        # Only process methods older than 730 days
-        if int(row['Age']) <= 730:
-            continue
-        
-        satd_list = row['SATD'].split('#')
-        age_list = row['ChangeAtMethodAge'].split('#')
+    for file in os.listdir(SRCDIR):
+        if file.endswith('.csv'):
+            fr = open(SRCDIR + file, "r")
+            line = fr.readline() # skip header
+            indexes = build_indexes(line)
+            lines = fr.readlines()
+            for line in lines:
+                row = line.strip().split("\t")
+                metric = row[indexes["SLOCStandard"]]
+                metric = metric.split("#")
+                metric = float(metric[0])
+                
+                if int(row[indexes['Age']]) <= 730:
+                    continue
+            
+                satd_list = row[indexes['SATD']].split('#')
+                age_list = row[indexes['ChangeAtMethodAge']].split('#')
 
-        start_index = None
+                start_index = None
 
-        for i, value in enumerate(satd_list):
-            if int(age_list[i]) > 730:
-                break
+                for i, value in enumerate(satd_list):
+                    if int(age_list[i]) > 730:
+                        break
 
-            if value == '1' and start_index is None:
-                start_index = i
-            elif value == '0' and start_index is not None:
-                end_index = i
-                start_age = int(age_list[start_index])
-                end_age = int(age_list[end_index])
-                removal_times.append(end_age - start_age)
-                removal_indices.append(index)
-                start_index = None  # Reset start index for next SATD segment
-
-    return removal_times, removal_indices
-
-def plot_cdf(removal_times):
-    # Calculate the CDF of the removal times
-    removal_times_sorted = np.sort(removal_times)
-    cdf = np.arange(1, len(removal_times_sorted) + 1) / len(removal_times_sorted)
-
-    # Plot the CDF
-    plt.figure(figsize=(10, 6))
-    plt.plot(removal_times_sorted, cdf, marker='o', linestyle='-', color='b')
-    plt.title('Cumulative Distribution Function (CDF) of SATD Removal Times')
-    plt.xlabel('Removal Time (days)')
-    plt.ylabel('CDF')
-    plt.grid(True)
-    # plt.show()
-
-    plt.savefig('satd_removal_cdf.pdf')
-    print("CDF plot saved as 'satd_removal_cdf.pdf'.")
-
-def process_files_in_folder(folder_path):
-    all_removal_times = []
-
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".csv"):
-            file_path = os.path.join(folder_path, filename)
-            df = pd.read_csv(file_path)
-            removal_times, _ = get_removal_times(df)
-            all_removal_times.extend(removal_times)
+                    if value == '1' and start_index is None:
+                        start_index = i
+                    elif value == '0' and start_index is not None:
+                        end_index = i
+                        start_age = int(age_list[start_index])
+                        end_age = int(age_list[end_index])
+                        removal_times.append(end_age - start_age)
+                        start_index = None  # Reset start index for next SATD segment   
     
-    return all_removal_times
+    return removal_times         
 
-def main():
-    folder_path = '/home/hisham-kidwai/Documents/HISHAM/Research/Tech-Debt/tech-debt-research/csv-results/part'
-    all_removal_times = process_files_in_folder(folder_path)
+def ecdf(a):
+    x, counts = np.unique(a, return_counts=True)
+    cusum = np.cumsum(counts)
+    return x, cusum / cusum[-1]
 
-    print("Total removal times from all files:", all_removal_times)
+def draw_graph(metrics):
+    # x, y = ecdf(metrics['satd'])
+    # ln = (plt.plot(x, y))
+    # plt.setp(ln, ls="-", linewidth=3, color='r', label='SATD')
 
-    plot_cdf(all_removal_times)
+    # x, y = ecdf(metrics['not_satd'])
+    # ln = (plt.plot(x, y))
+    # plt.setp(ln, ls="--", linewidth=3, color='blue', label='NOT_SATD')
 
-if __name__ == '__main__':
-    main()
+    x, y = ecdf(metrics)
+    ln = plt.plot(x,y)
+    plt.setp(ln, ls="-", linewidth=3, color='r', label='Removal Times')
+    plt.legend()
+    plt.xlabel("MI")
+    plt.ylabel("CDF")
+    # plt.xlim(0, 100)
+    plt.show()
+
+if __name__ == "__main__":
+    metrics  = process()
+    draw_graph(metrics)
+
