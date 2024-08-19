76916ba8165d9a59d69c3acb131439f412eaca9b
Hisham Kidwai <HishamSurgeon@gmail.com>
2024-08-18 19:48:28 -0500

    Revert back to before

--- rq2/code-metric-distribution.py
+++ rq2/code-metric-distribution.py
@@ -2,7 +2,12 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+# SCALE = 'log'
+# METRIC_VALUE = 'mean' # first, mean, last
+
+# SRCDIR = "../../software-evolution/tech-debt-results/"
 SRCDIR = '/home/hisham-kidwai/Documents/HISHAM/Research/Tech-Debt/csv-files-satd/'
+# SRCDIR = '/home/hisham-kidwai/Documents/HISHAM/Research/Tech-Debt/tech-debt-research/csv-results/full/'
 
 def build_indices(line):
     indexes = {}
@@ -24,51 +29,66 @@ def find_index_to_stop(age_list: list):
             return i
     return -1
 
-def get_top_5_largest_files(srcdir):
-    files_with_size = [(file, os.path.getsize(os.path.join(srcdir, file))) 
-                       for file in os.listdir(srcdir) if file.endswith('.csv')]
-    sorted_files = sorted(files_with_size, key=lambda x: x[1], reverse=True)
-    return [file[0] for file in sorted_files[:5]]
-
-def process(metrics_list, METRIC_VALUE, file):
+def process(metrics_list, METRIC_VALUE):
+    """
+    Have the different metrics in a dictionary
+    Each key will be a metric and the value will be a dictionary with 'satd' and 'not_satd' lists
+    """
     metrics = {metric: {'satd': [], 'not_satd': []} for metric in metrics_list}
     
-    fr = open(os.path.join(SRCDIR, file), "r")
-    line = fr.readline()  # skip header
-    indices = build_indices(line)
-    lines = fr.readlines()
+    for file in os.listdir(SRCDIR):
+        if file.endswith('.csv'):
+            fr = open(SRCDIR + file, "r")
+            line = fr.readline()  # skip header
+            indices = build_indices(line)
+            lines = fr.readlines()
 
-    for line in lines:
-        row = line.strip().split("\t")
+            # counter = 0
 
-        if int(row[indices['Age']]) > 730: # Make sure the method is at least 2 years old     
-            for metric_name in metrics_list:
-                metric = row[indices[metric_name]].split("#")
-                age_list = row[indices['ChangeAtMethodAge']].split('#')
-                satd = row[indices["SATD"]].split("#")
+            for line in lines:
+                # counter += 1
+                # if counter > 100:
+                #     break
 
-                index_to_stop = find_index_to_stop(age_list)
-                if index_to_stop != -1: # if index_to_stop is -1, then there are no commits with age > 730 so keep as is
-                    metric = metric[:index_to_stop]
-                    satd = satd[:index_to_stop]
+                row = line.strip().split("\t")
 
-                if METRIC_VALUE == 'first':
-                    metric = float(metric[0])
-                elif METRIC_VALUE == 'mean':
-                    metric = [float(m) for m in metric]
-                    metric = np.mean(metric)
-                elif METRIC_VALUE == 'last':
-                    metric = float(metric[-1])
-                else:
-                    print('ENTER A VALID METRIC VALUE')
-                
-                if metric < 0:
-                    print('NEGATIVE METRIC VALUE')
-                    continue  # something is wrong
-                if check_satd(satd):
-                    metrics[metric_name]['satd'].append(metric)
-                else:
-                    metrics[metric_name]['not_satd'].append(metric)
+                if int(row[indices['Age']]) > 730: # Make sure the method is at least 2 years old     
+                    for metric_name in metrics_list:
+                        metric = row[indices[metric_name]].split("#")
+                        age_list = row[indices['ChangeAtMethodAge']].split('#')
+                        satd = row[indices["SATD"]].split("#")
+
+                        index_to_stop = find_index_to_stop(age_list)
+                        if index_to_stop != -1: # if index_to_stop is -1, then there are no commits with age > 730 so keep as is
+                                metric = metric[:index_to_stop]
+                                satd = satd[:index_to_stop]
+
+                        if METRIC_VALUE == 'first':
+                            # print(metric)
+                            metric = float(metric[0])
+                            # print(file, row[indices['file']])
+                            # print(metric)
+                        elif METRIC_VALUE == 'mean':
+                            # print(file, row[indices['file']])
+                            # print(metric)
+                            metric = [float(m) for m in metric]
+                            metric = np.mean(metric)
+                            # print(metric)
+                        elif METRIC_VALUE == 'last':
+                            # print(metric)
+                            metric = float(metric[-1])
+                            # print(file, row[indices['file']])
+                            # print(metric)
+                        else:
+                            print('ENTER A VALID METRIC VALUE')
+                        
+                        if metric < 0:
+                            print('NEGATIVE METRIC VALUE')
+                            continue  # something is wrong
+                        if check_satd(satd):
+                            metrics[metric_name]['satd'].append(metric)
+                        else:
+                            metrics[metric_name]['not_satd'].append(metric)
     return metrics
 
 def ecdf(a):
@@ -76,7 +96,7 @@ def ecdf(a):
     cusum = np.cumsum(counts)
     return x, cusum / cusum[-1]
 
-def draw_graph(metrics, METRIC_VALUE, SCALE, file_name):
+def draw_graph(metrics, METRIC_VALUE, SCALE):
     for metric_name, data in metrics.items():
         plt.figure()
         
@@ -103,31 +123,42 @@ def draw_graph(metrics, METRIC_VALUE, SCALE, file_name):
             elif metric_name == 'MaintainabilityIndex':
                 pass
             elif metric_name == 'McCabe':
+                # pass
                 plt.xlim(0, 75)
             elif metric_name == 'totalFanOut':
+                # pass
                 plt.xlim(0, 100)
             elif metric_name == 'uniqueFanOut':
+                # pass
                 plt.xlim(0, 75)
             
+        # plt.xlim(0, 1)
         plt.title(f"CDF of {metric_name}")
-
-        output_dir = f'figs/rq2/{METRIC_VALUE}/{file_name}/{SCALE}'
-        os.makedirs(output_dir, exist_ok=True)
-
+        # plt.show()
         if METRIC_VALUE == 'first':
-            plt.savefig(f'{output_dir}/f_{metric_name}.pdf')
+            if SCALE == 'log':
+                plt.savefig(f'figs/rq2/first/log/f_{metric_name}.pdf')
+            else:
+                plt.savefig(f'figs/rq2/first/linear/f_{metric_name}.pdf')
         elif METRIC_VALUE == 'mean':
-            plt.savefig(f'{output_dir}/m_{metric_name}.pdf')
+            if SCALE == 'log':
+                plt.savefig(f'figs/rq2/mean/log/m_{metric_name}.pdf')
+            else:
+                plt.savefig(f'figs/rq2/mean/linear/m_{metric_name}.pdf')
         elif METRIC_VALUE == 'last':
-            plt.savefig(f'{output_dir}/l_{metric_name}.pdf')
+            if SCALE == 'log':
+                plt.savefig(f'figs/rq2/last/log/l_{metric_name}.pdf')
+            else:
+                plt.savefig(f'figs/rq2/last/linear/l_{metric_name}.pdf')
 
 if __name__ == "__main__":
     metrics_list = ['SLOCStandard', 'Readability', 'SimpleReadability', 'MaintainabilityIndex', 'McCabe', 'totalFanOut', 'uniqueFanOut']
-    
-    top_files = get_top_5_largest_files(SRCDIR)
-
+    # metrics_list = ['SLOCStandard']
+    # metrics = process(metrics_list)
+    # draw_graph(metrics)
+    # Loop over METRIC_VALUE and SCALE
     for METRIC_VALUE in ['first', 'mean', 'last']:
         for SCALE in ['log', 'linear']:
-            for file_name in top_files:
-                metrics = process(metrics_list, METRIC_VALUE, file_name)
-                draw_graph(metrics, METRIC_VALUE, SCALE, file_name)
+            metrics = process(metrics_list, METRIC_VALUE)
+            draw_graph(metrics, METRIC_VALUE, SCALE)
+
