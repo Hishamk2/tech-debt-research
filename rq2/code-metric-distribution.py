35dd2a3fcf55d370f4357995379ef96547fac04e
Hishamk <67921181+Hishamk2@users.noreply.github.com>
2024-08-31 19:45:08 -0500

    Print individual project Cliff Delta if opposite sign

--- rq2/code-metric-distribution.py
+++ rq2/code-metric-distribution.py
@@ -1,9 +1,41 @@
 import os
 import numpy as np
-from cliffs_delta import cliffs_delta  # Assuming this is already installed and working
+from cliffs_delta import cliffs_delta
 from scipy.stats import mannwhitneyu
 
 SRCDIR = r'D:\\OneDrive - University of Manitoba\\Documents\\HISHAM\\Research\\Tech-Debt\\csv-files-satd\\'
+
+EXPECTED_SIGNS = {
+    'SLOCAsItIs': 0.65,
+    'SLOCNoCommentPrettyPrinter': 0.54,
+    'SLOCStandard': 0.56,
+    'CommentCodeRation': 0.72,
+    'Readability': -0.37,
+    'SimpleReadability': -0.49,
+    'NVAR': 0.46,
+    'NCOMP': 0.46,
+    'Mcclure': 0.46,
+    'McCabe': 0.50,
+    'McCabeWOCases': 0.49,
+    'IndentSTD': 0.36,
+    'MaximumBlockDepth': 0.47,
+    'totalFanOut': 0.52,
+    'uniqueFanOut': 0.52,
+    'n1': 0.56,
+    'n2': 0.55,
+    'N1': 0.57,
+    'N2': 0.55,
+    'Vocabulary': 0.57,
+    'Length': 0.56,
+    'Volume': 0.56,
+    'Difficulty': 0.54,
+    'Effort': 0.56,
+    'Time': 0.56,
+    'HalsteadBugs': 0.56,
+    'MaintainabilityIndex': -0.57,
+    'Parameters': 0.26,
+    'LocalVariables': 0.49,
+}
 
 def build_indices(line):
     indexes = {}
@@ -28,41 +60,41 @@ def find_index_to_stop(age_list: list):
 def process_project(file, metrics_list, METRIC_VALUE):
     metrics = {metric: {'satd': [], 'not_satd': []} for metric in metrics_list}
     
-    fr = open(SRCDIR + file, "r", encoding='utf-8')
-    line = fr.readline()  # skip header
-    indices = build_indices(line)
-    lines = fr.readlines()
+    with open(SRCDIR + file, "r", encoding='utf-8') as fr:
+        line = fr.readline()  # skip header
+        indices = build_indices(line)
+        lines = fr.readlines()
 
-    for line in lines:
-        row = line.strip().split("\t")
+        for line in lines:
+            row = line.strip().split("\t")
 
-        if int(row[indices['Age']]) > 730: # Make sure the method is at least 2 years old     
-            for metric_name in metrics_list:
-                metric = row[indices[metric_name]].split("#")
-                age_list = row[indices['ChangeAtMethodAge']].split('#')
-                satd = row[indices["SATD"]].split("#")
+            if int(row[indices['Age']]) > 730:  # Ensure the method is at least 2 years old
+                for metric_name in metrics_list:
+                    metric = row[indices[metric_name]].split("#")
+                    age_list = row[indices['ChangeAtMethodAge']].split('#')
+                    satd = row[indices["SATD"]].split("#")
 
-                index_to_stop = find_index_to_stop(age_list)
-                if index_to_stop != -1: # if index_to_stop is -1, then there are no commits with age > 730 so keep as is
+                    index_to_stop = find_index_to_stop(age_list)
+                    if index_to_stop != -1:  # If index_to_stop is -1, then there are no commits with age > 730
                         metric = metric[:index_to_stop]
                         satd = satd[:index_to_stop]
 
-                if METRIC_VALUE == 'first':
-                    metric = float(metric[0])
-                elif METRIC_VALUE == 'mean':
-                    metric = [float(m) for m in metric]
-                    metric = np.mean(metric)
-                elif METRIC_VALUE == 'last':
-                    metric = float(metric[-1])
-                else:
-                    print('ENTER A VALID METRIC VALUE')
+                    if METRIC_VALUE == 'first':
+                        metric = float(metric[0])
+                    elif METRIC_VALUE == 'mean':
+                        metric = [float(m) for m in metric]
+                        metric = np.mean(metric)
+                    elif METRIC_VALUE == 'last':
+                        metric = float(metric[-1])
+                    else:
+                        print('ENTER A VALID METRIC VALUE')
 
-                if metric < 0:
-                    continue  # something is wrong
-                if check_satd(satd):
-                    metrics[metric_name]['satd'].append(metric)
-                else:
-                    metrics[metric_name]['not_satd'].append(metric)
+                    if metric < 0:
+                        continue  # something is wrong
+                    if check_satd(satd):
+                        metrics[metric_name]['satd'].append(metric)
+                    else:
+                        metrics[metric_name]['not_satd'].append(metric)
     
     return metrics
 
@@ -78,36 +110,21 @@ def categorize_cliffs_delta(delta):
         return 'L'  # Large
 
 def analyze_projects(metrics_list, METRIC_VALUE='first'):
-    # Initialize the summary table
-    summary = {metric: {'N': 0, 'S': 0, 'M': 0, 'L': 0, 'total': 0} for metric in metrics_list}
-
     for file in os.listdir(SRCDIR):
         if file.endswith('.csv'):
             metrics = process_project(file, metrics_list, METRIC_VALUE)
-            # print(metrics)
             for metric_name, data in metrics.items():
                 if len(data['satd']) > 0 and len(data['not_satd']) > 0:
                     delta, magnitude = cliffs_delta(data['satd'], data['not_satd'])
-                    # print(f"File: {file}, Metric: {metric_name}, Delta: {delta}, Magnitude: {magnitude}")
-                    category = categorize_cliffs_delta(delta)
-                    # print(f"File: {file}, Metric: {metric_name}, Category: {category}")
-                    summary[metric_name][category] += 1
-                    summary[metric_name]['total'] += 1
-                    # print(f"Updated summary for {metric_name}: {summary[metric_name]}")
-
-
-            # print(summary)
-    # Print the summary table
-    print(f"{'Metric':<20} {'N (%)':<10} {'S (%)':<10} {'M (%)':<10} {'L (%)':<10}")
-    print("=" * 60)
-    for metric_name, counts in summary.items():
-        if counts['total'] > 0:
-            n_percent = (counts['N'] / counts['total']) * 100
-            s_percent = (counts['S'] / counts['total']) * 100
-            m_percent = (counts['M'] / counts['total']) * 100
-            l_percent = (counts['L'] / counts['total']) * 100
-            print(f"{metric_name:<20} {n_percent:<10.0f} {s_percent:<10.0f} {m_percent:<10.0f} {l_percent:<10.0f}")
+                    expected_delta = EXPECTED_SIGNS[metric_name]
+                    # Check if the sign matches the expected delta
+                    if (delta < 0 and expected_delta > 0) or (delta > 0 and expected_delta < 0):
+                        print(f"{file} {metric_name} Cliff Delta = {delta:.2f}, expected = {expected_delta:.2f}")
 
 if __name__ == "__main__":
-    metrics_list = ['SLOCStandard', 'Readability', 'SimpleReadability', 'MaintainabilityIndex', 'totalFanOut']
+    metrics_list = ['SLOCAsItIs', 'SLOCNoCommentPrettyPrinter', 'SLOCStandard', 'CommentCodeRation', 'Readability', 
+                    'SimpleReadability', 'NVAR', 'NCOMP', 'Mcclure', 'McCabe', 'McCabeWOCases', 'IndentSTD', 
+                    'MaximumBlockDepth', 'totalFanOut', 'uniqueFanOut', 'n1', 'n2', 'N1', 'N2', 'Vocabulary', 
+                    'Length', 'Volume', 'Difficulty', 'Effort', 'Time', 'HalsteadBugs', 'MaintainabilityIndex', 
+                    'Parameters', 'LocalVariables']
     analyze_projects(metrics_list, METRIC_VALUE='first')
